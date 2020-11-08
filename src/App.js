a2f4af590bea21db0946a5c05efaec9d14d5bcf2
Aravinda Tangirala <atangirala@inniveinc.com>
2020-11-08 08:24:17 -0500

    homepage update commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import Feedback from "./components/Feedback"
-import { useHistory,BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import Feedback from "./components/Feedback";
+import {
+  useHistory,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import FeedbackConfirmation from "./components/FeedbackConfirmation";
 import FeedbackSummary from "./components/FeedbackSummary";
-import './App.css';
+import "./App.css";
 
 function App() {
- const  history=useHistory();
+  const history = useHistory();
   return (
     <Router>
-       <div className="App">
-       <Switch> 
-       {/* <Route path ="/">
+      <div className="App">
+        <Switch>
+          {/* <Route path ="/">
           {history.push("/feedback")}
        </Route> */}
-       <Route path ="/feedback_submit" exact>
-         <FeedbackConfirmation />
-       </Route>
-       <Route path ="/feedback_summary" exact>
-         <FeedbackSummary />
-       </Route>
-       <Route path ="/feedback">
-         <Feedback/>
-       </Route>
-       
-       </Switch>
-   
-        
-    </div>
-  
+          <Redirect from="/" to="/feedback" />
+          <Route path="/feedback_submit" exact>
+            <FeedbackConfirmation />
+          </Route>
+          <Route path="/feedback_summary" exact>
+            <FeedbackSummary />
+          </Route>
+          <Route path="/feedback">
+            <Feedback />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
-   
 }
 
 export default App;
